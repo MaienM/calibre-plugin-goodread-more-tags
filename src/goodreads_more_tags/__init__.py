cc843166db21bb35380ba91c3a3aef4f631f3d7a
Michon van Dooren <michon1992@gmail.com>
2019-08-04 15:45:22 +0200

    Tagging v1.0.1

--- src/goodreads_more_tags/__init__.py
+++ src/goodreads_more_tags/__init__.py
@@ -15,7 +15,7 @@ class GoodreadsMoreTags(Source):
     description = 'Scrape more tags from Goodreads'
     author = 'Michon van Dooren'
 
-    version = (1, 0, 0)
+    version = (1, 0, 1)
     minimum_calibre_version = (0, 7, 53)
     supported_platforms = ['windows', 'osx', 'linux']
 
