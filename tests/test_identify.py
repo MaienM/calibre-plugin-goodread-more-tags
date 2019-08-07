9f3ce79dd647a073f79bffd433466cfbeae524e0
Michon van Dooren <michon1992@gmail.com>
2019-08-07 12:28:03 +0200

    Added more identify tests

--- tests/test_identify.py
+++ tests/test_identify.py
@@ -16,12 +16,25 @@ class TestIdentify(object):
     def test_goodreads_id(self, identify):
         results = identify(plugins = [GoodreadsMoreTags], identifiers = { 'goodreads': '902715' })
         assert len(results) == 1
-        assert sorted(results[0].tags) == [
-            'Adventure',
-            'Fantasy',
-            'Science Fiction',
-            'War',
-        ]
+        assert sorted(results[0].tags) == ['Adult', 'Adventure', 'Fantasy', 'Fiction', 'Science Fiction', 'War']
+
+    def test_goodreads_id_high_absolute(self, configs, identify):
+        configs.goodreads_more_tags.treshold_absolute = 250
+        results = identify(plugins = [GoodreadsMoreTags], identifiers = { 'goodreads': '902715' })
+        assert len(results) == 1
+        assert sorted(results[0].tags) == ['Fantasy', 'Fiction']
+
+    def test_goodreads_id_high_percentage(self, configs, identify):
+        configs.goodreads_more_tags.treshold_percentage = 75
+        results = identify(plugins = [GoodreadsMoreTags], identifiers = { 'goodreads': '902715' })
+        assert len(results) == 1
+        assert sorted(results[0].tags) == ['Adventure', 'Fantasy', 'Fiction', 'Science Fiction']
+
+    def test_goodreads_id_percentage_of(self, configs, identify):
+        configs.goodreads_more_tags.treshold_percentage_of = [1, 3]
+        results = identify(plugins = [GoodreadsMoreTags], identifiers = { 'goodreads': '902715' })
+        assert len(results) == 1
+        assert sorted(results[0].tags) == ['Fantasy', 'Fiction']
 
     def test_isbn(self, identify):
         results = identify(plugins = [GoodreadsMoreTags], identifiers = { 'isbn': '9780575077881' })
