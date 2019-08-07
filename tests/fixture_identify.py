814259968365c900117370b0fec373f74d6d186b
Michon van Dooren <michon1992@gmail.com>
2019-08-07 12:52:31 +0200

    tests: Enable plugins that are used in identify

--- tests/fixture_identify.py
+++ tests/fixture_identify.py
@@ -10,12 +10,15 @@ import pytest
 
 @pytest.fixture
 def identify():
+    from calibre.customize.ui import enable_plugin
     from calibre.ebooks.metadata.sources.identify import identify as calibre_identify
     from calibre.ebooks.metadata.sources.test import create_log
 
     def identify(plugins = None, **kwargs):
         """ Run an identify with the given options, and return the list of results. """
         if plugins:
+            for plugin in plugins:
+                enable_plugin(plugin)
             kwargs['allowed_plugins'] = [p.name for p in plugins]
 
         # Get required parameters to run identify.
