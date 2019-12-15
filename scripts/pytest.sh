031411972f61b4721ae66afc982dae0e8e7fbf03
Michon van Dooren <michon1992@gmail.com>
2019-12-15 15:52:59 +0100

    Use all available procs for xdist, if useful
    There is no sense in spinning up more test runners than tests, so avoid
    doing so.

---
+++ scripts/pytest.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env sh
+
+./scripts/kill-and-run.sh
+
+echo "===== Pytest ====="
+MIN_TESTS_PER_PROC=2
+(
+	set -e
+
+	numtests="$(pytest --collect-only "$@" | grep -Ex 'collected [0-9]+ items?' | cut -d' ' -f2)"
+	numprocs="$(grep -c '^processor' /proc/cpuinfo)"
+	useprocs="$((numtests / MIN_TESTS_PER_PROC))"
+	[ "$useprocs" -le "$numprocs" ] || useprocs="$numprocs"
+	[ "$useprocs" -gt 0 ] || useprocs=1
+
+	pytest -vv --numprocesses="$useprocs" "$@"
+)
